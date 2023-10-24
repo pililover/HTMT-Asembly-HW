8ce1f097d77646eb73708c1cb58c085ae2842af4
pililover <118993806+pililover@users.noreply.github.com>
2023-10-24 15:57:37 +0700

    ex1

---
+++ ex1.asm
@@ -0,0 +1,55 @@
+.data
+num1_mes: .asciiz "Input a: "
+num2_mes: .asciiz "Input b: "
+
+case1: .asciiz "a>b"
+case2: .asciiz "a<b"
+case3: .asciiz "a=b"
+
+.text
+main:
+	li $v0, 4
+	la $a0, num1_mes
+	syscall
+
+	li $v0, 5
+	syscall 
+	move $t0, $v0
+
+	li $v0, 4
+	la $a0, num2_mes
+	syscall
+
+	li $v0, 5
+	syscall
+	move $t1, $v0
+
+	beq $t0, $t1, printRes
+
+	slt $t2, $t0, $t1
+	bne $t2, $0, printResult
+
+	li $v0, 4
+	la $a0, case1
+	syscall
+	j END
+
+	printRes:
+		li $v0, 4
+		la $a0, case3
+		syscall
+		j END
+
+	printResult:
+		li $v0, 4
+		la $a0, case2
+		syscall
+
+END:
+	addi $a0, $0, 0
+	addi $v0, $0, 17
+	syscall
+	
+
+	
+	
